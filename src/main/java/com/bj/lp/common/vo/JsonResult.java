fac3aad5f4f10ff919818169975289135317d22a
laocai <3360845054@qq.com>
2019-10-17 19:23:48 +0800

    爱旅拍

---
+++ src/main/java/com/bj/lp/common/vo/JsonResult.java
@@ -0,0 +1,27 @@
+package com.bj.lp.common.vo;
+
+import java.io.Serializable;
+
+public class JsonResult implements Serializable {
+    private static final long serialVersionUID = 874154493505251116L;
+//    状态码 1表示success，0表示error
+    private  int state=1;
+//    状态信息
+    private  String message="ok";
+//    正确数据
+    private  Object data;
+
+
+    public JsonResult(String message) {
+        this.message = message;
+    }
+
+    public JsonResult(Object data) {
+        this.data = data;
+    }
+
+    public JsonResult(int state, String message) {
+        this.state = state;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/bj/lp/common/web/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.bj.lp.common.web;
+
+import com.bj.lp.common.vo.JsonResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public JsonResult doHandleRuntimeException(RuntimeException e) {
+        e.printStackTrace();
+        return  new JsonResult(e);
+    }
+}
