827bd14aacbc476ab36da157a7d72566c722e1aa
laocai <3360845054@qq.com>
2019-10-17 19:23:48 +0800

    爱旅拍

---
+++ src/main/java/com/bj/lp/BojueApplication.java
@@ -0,0 +1,14 @@
+package com.bj.lp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BojueApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BojueApplication.class, args);
+    }
+
+}
