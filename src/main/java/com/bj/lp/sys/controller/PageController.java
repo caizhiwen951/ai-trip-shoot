224094a198656481fc8dc36b6d19a93f16c6c5a9
frog-git <yucheng_oil@163.com>
2019-10-20 10:12:25 +0800

    没用

--- src/main/java/com/bj/lp/sys/controller/PageController.java
+++ src/main/java/com/bj/lp/sys/controller/PageController.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class PageController {
 
-    /**返回分页页面*/
-    @RequestMapping("doPageUI")
-    public String doPageUI() {
-        return "common/page";
-    }
+
 
     @RequestMapping("{moduleUI}")
     public String doModuleUI(
