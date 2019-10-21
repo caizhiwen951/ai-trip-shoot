600c95c10a429fafb25a536c7cb49931b188e2a1
zzf <542583957@qq.com>
2019-10-21 21:31:52 +0800

    注册登录 -zzf

--- src/main/java/com/bj/lp/sys/controller/PageController.java
+++ src/main/java/com/bj/lp/sys/controller/PageController.java
@@ -12,15 +12,12 @@ public class PageController {
     @RequestMapping("{moduleUI}")
     public String doModuleUI(
             @PathVariable String moduleUI) {
-        System.out.println(123);
-        System.out.println(111);
         return moduleUI;
     }
     @RequestMapping("index")
     public String doIndexUI() {
         return "index";
     }
-
 
     @RequestMapping("chatUI")
     public String chatUI(){
